7dedb258e68a1ad16b279ca30d790b9ec939f58e
crow <646612780@qq.com>
2019-11-27 18:23:43 +0800

    Add files via upload
    sample

---
+++ reg_digital.py
@@ -0,0 +1,38 @@
+import re 
+class RegDigital:
+    '''this class  obtains regualar expression about digital'''
+    @staticmethod
+    def match_digital(inputed_str):
+        '''this is a sample ,print the result you test
+
+        inputed_str:the data you want to be test 
+        '''
+
+        #include: positive integer(0 not included) eg: 1 2 3 4
+        #([1-9][0-9]*)
+
+        #include:integer(0 include) 、decimal eg:-1   0     1   2   3   4
+        #(-?(0|([1-9][0-9]*)))
+
+        #include:integer(0 include) 、decimal eg:-1   0     1    0.2    2.3    2.01
+        #(-?((0|([1-9][0-9]*))(\.[0-9]*[1-9])?))  
+
+        pattern = r''   #replace your regular expression
+        regex = re.compile(pattern)
+        for i in regex.finditer(inputed_str):
+            print(i.group())
+
+
+
+if __name__ == "__main__":
+    RegDigital.match_digital('''Hello World -12.6
+Nihao  123
+How are you  -12
+1.24
+asdk 34%,
+precent 1/2
+2003 - 2005
+0
+0.123
+0123
+123/-0.2313''')
