a471809039b654f8f973cae98e874d7bbc2f9834
crow <646612780@qq.com>
2019-11-27 19:28:53 +0800

    Update reg_digital.py

--- reg_digital.py
+++ reg_digital.py
@@ -7,7 +7,8 @@
 #include:integer(0 include) 、decimal eg:-1   0     1    0.2    2.3    2.01
 #(-?((0|([1-9][0-9]*))(\.[0-9]*[1-9])?))  
 
-
+#include:integer(0 include) 、decimal、percentage eg:-1   0  0.2/0.3    2.3    2/1  
+#(-?((0|([1-9][0-9]*))(\.[0-9]*[1-9])?))(/(-?(0\.[0-9]*[1-9])|([1-9][0-9]*(\.[0-9]*[1-9])?)))?
 #-----------------  sample following ------------------------------------
 import re 
 class RegDigital:
